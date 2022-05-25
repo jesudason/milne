d8823574d5c405350ecde007dfb330fd5265e812
Nicole Jesudason <nicole.jesudason@gmail.com>
2022-05-25 22:10:25 +1000

    update client details

--- src/Client.js
+++ src/Client.js
@@ -1,7 +1,7 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  projectId: "idyp9rct", // find this at manage.sanity.io or in your sanity.json
+  projectId: "fdqaoni1", // find this at manage.sanity.io or in your sanity.json
   dataset: "production", // this is from those question during 'sanity init'
   useCdn: true,
 });
