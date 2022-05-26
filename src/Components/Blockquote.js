3600897f57bb9e014c1716aaaa24b33666913659
Nicole Jesudason <nicole.jesudason@gmail.com>
2022-05-26 22:10:43 +1000

    blockquote styling

--- src/Components/Blockquote.js
+++ src/Components/Blockquote.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./Blockquote.scss";
+import variables from "../variables.scss";
 
 export const Blockquote = (props) => {
+  console.log(props);
   return (
     <div
       id={props.component._key}
       className="Blockquote full-width"
-      style={{ backgroundColor: props.component.hexCode }}
+      style={{ backgroundColor: variables[props.component.backgroundColor] }}
     >
-      <blockquote>
+      <blockquote
+        style={{
+          color:
+            props.component.backgroundColor === "white"
+              ? variables.black
+              : variables.white,
+        }}
+      >
         {props.component.quote}
         <span>{props.component.author}</span>
       </blockquote>
