90ea304d37c4d3c0951a2e4498e9c6cc4b54088d
Nicole Jesudason <nicole.jesudason@gmail.com>
2022-05-29 11:00:06 +1000

    banner styled

--- src/Layouts/PageBanner.js
+++ src/Layouts/PageBanner.js
@@ -5,38 +5,52 @@ import { urlFor } from "../helpers";
 export const PageBanner = ({ props }) => {
   const bannerImageURL = props.mainImage ? urlFor(props.mainImage).url() : "";
   return (
-    <div
-      className="PageBanner full-width"
-      style={{
-        backgroundImage: bannerImageURL
-          ? "url(" + bannerImageURL + ")"
-          : "none",
-      }}
-    >
+    <div className="PageBanner">
       {props.pageType === "index" ? (
-        <div className="PageBanner__hero">
-          <div className="PageBanner__text">
-            <h1 className="PageBanner__text--title">{props.title}</h1>
-            {props.subtitle ? (
-              <p className="PageBanner__text--subtitle">{props.subtitle}</p>
-            ) : (
-              ""
-            )}
+        <section className="hero-banner">
+          <div
+            className="hero-banner__background"
+            style={{
+              backgroundImage: bannerImageURL
+                ? "url(" + bannerImageURL + ")"
+                : "none",
+            }}
+          >
+            <span className="hero-banner__background-blur"></span>
           </div>
-        </div>
+          <div className="hero-banner__text">
+            <div className="container">
+              <h1 className="PageBanner__text--title">{props.title}</h1>
+              {props.subtitle ? (
+                <p className="PageBanner__text--subtitle">{props.subtitle}</p>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+        </section>
       ) : (
-        <div className="PageBanner__skinny">
-          <div className="PageBanner__text">
-            <h2 className="PageBanner__text--title">{props.title}</h2>
-            {props.subtitle ? (
-              <h3 className="PageBanner__text--subtitle">{props.subtitle}</h3>
-            ) : (
-              ""
-            )}
+        <section className="banner banner__type-skinny">
+          <div className="container">
+            <div
+              className="banner__background"
+              style={{
+                backgroundImage: bannerImageURL
+                  ? "url(" + bannerImageURL + ")"
+                  : "none",
+              }}
+            ></div>
+            <div className="banner__details">
+              <h1 className="PageBanner__text--title">{props.title}</h1>
+              {props.subtitle ? (
+                <h2 className="PageBanner__text--subtitle">{props.subtitle}</h2>
+              ) : (
+                ""
+              )}
+            </div>
           </div>
-        </div>
+        </section>
       )}
-      <div className="PageBanner__overlay"></div>
     </div>
   );
 };
